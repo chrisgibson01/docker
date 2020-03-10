6221027c7aeff69d6557350cd0e2a534aabc67ec
Chris Gibson <chris@gibsonic.com>
2020-03-10 10:53:47 +0000

    txblaster added

--- bsv_dev/run.sh
+++ bsv_dev/run.sh
@@ -1,9 +1,9 @@
 !/bin/bash
 
 docker run --volumes-from vc_sv \
-           --volumes-from vc_sdk_libraries \
            --privileged \
            --publish 8332:8332 \
            --publish 18332:18332 \
            -it bsv_dev /bin/bash
+           #--volumes-from vc_sdk_libraries \
 
--- bsv_dev/run_vc_sv.sh
+++ bsv_dev/run_vc_sv.sh
@@ -1,3 +1,3 @@
 docker run -it \
-	--mount type=bind,src=/home/ads.nchain.com/c.gibson/bb/sv,dst=/root/sv \
+	--mount type=bind,src=/root/sv,dst=/root/sv \
 	--name vc_sv ubuntu_dev /bin/bash
