e0f21aad9dacb866058d70e644ae57583bc41154
Chris Gibson <chris@gibsonic.com>
2020-03-10 16:51:05 +0000

    adding bitcoin.conf

--- bsv_dev/run.sh
+++ bsv_dev/run.sh
@@ -4,6 +4,7 @@ docker run --volumes-from vc_sv \
            --privileged \
            --publish 8332:8332 \
            --publish 18332:18332 \
+           --publish 18444:18444 \
            -it bsv_dev /bin/bash
            #--volumes-from vc_sdk_libraries \
 
