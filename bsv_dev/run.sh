8b65a4eb01747817913844504158e909d7e7ad98
Chris Gibson <chris@gibsonic.com>
2020-03-12 14:40:06 +0000

    bsv_dev as host network

--- bsv_dev/run.sh
+++ bsv_dev/run.sh
@@ -2,9 +2,14 @@
 
 docker run --volumes-from vc_sv \
            --privileged \
-           --publish 8332:8332 \
-           --publish 18332:18332 \
-           --publish 18444:18444 \
+	   --net host \
            -it bsv_dev /bin/bash
-           #--volumes-from vc_sdk_libraries \
+
+#docker run --volumes-from vc_sv \
+#           --privileged \
+#           --publish 8332:8332 \
+#           --publish 18332:18332 \
+#           --publish 18444:18444 \
+#           -it bsv_dev /bin/bash
+#           #--volumes-from vc_sdk_libraries \
 
