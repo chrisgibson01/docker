8251a9b3f45ac4609744f9f77c79961338381f2c
Chris Gibson <chris@gibsonic.com>
2020-03-23 13:05:28 +0000

    dev_bsv_ubuntu

---
+++ dev_bsv_ubuntu_19.10/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#docker run --volumes-from vc_sv \
+#           -it bsv_dev_ubuntu:18.04 /bin/bash
+
+docker run --volumes-from vc_sv \
+           --privileged \
+    	   --net host \
+           -it bsv_dev_ubuntu:19.10 /bin/bash
+
+#docker run --volumes-from vc_sv \
+#           --privileged \
+#           --publish 8332:8332 \
+#           --publish 18332:18332 \
+#           --publish 18444:18444 \
+#           -it bsv_dev /bin/bash
+#           #--volumes-from vc_sdk_libraries \
+
