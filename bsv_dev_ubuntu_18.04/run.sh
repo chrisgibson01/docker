308ca4d69085e26ed2064362f7b423d43aab8e38
Chris Gibson <chris@gibsonic.com>
2020-05-13 13:48:16 +0000

    bsv_centos_7 updates

--- bsv_dev_ubuntu_18.04/run.sh
+++ bsv_dev_ubuntu_18.04/run.sh
@@ -10,6 +10,8 @@
 
 docker run --volumes-from vc_sv \
            --privileged \
+            --cap-add=SYS_ADMIN \
+            --volume /lib/modules:/lib/modules:ro \
            --publish 8332:8332 \
            --publish 18332:18332 \
            --publish 18444:18444 \
