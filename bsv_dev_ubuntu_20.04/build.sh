c19d0991d2042de7fdd7cc455074daeda972d5cc
Chris Gibson <chris@gibsonic.com>
2020-09-25 17:42:16 +0100

    gdb added

--- bsv_dev_ubuntu_20.04/build.sh
+++ bsv_dev_ubuntu_20.04/build.sh
@@ -5,6 +5,6 @@ cat ../common/ubuntu_20.04 \
     ../common/build_tools \
     ../common/clang \
     ../common/misc \
-    ../common/github \
+    ../common/config \
     dockerfile \
     | docker build -t bsv_dev_ubuntu:20.04 -
