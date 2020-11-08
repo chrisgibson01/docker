3fa96f6280d6a147833f1aa7b2dacc9a664ca8f3
Chris Gibson <chris@gibsonic.com>
2020-11-08 18:23:52 +0000

    electrum_sv

--- bsv_dev_ubuntu_20.04/run.sh
+++ bsv_dev_ubuntu_20.04/run.sh
@@ -1,24 +1,10 @@
-#sudo docker run --privileged -it --rm bsv_dev_ubuntu:20.04 /bin/bash
-
 docker run --volumes-from vc_sv \
            --privileged \
             --cap-add=SYS_ADMIN \
             --volume /lib/modules:/lib/modules:ro \
-           --publish 8332:8332 \
            --publish 18332:18332 \
-           --publish 18444:18444 \
-           --publish 11150:11150 \
-           --publish 11151:11151 \
            -it \
            --rm \
            --net bridge \
            bsv_dev_ubuntu:20.04 /bin/bash
 
-#docker run --volumes-from vc_sv \
-#           --privileged \
-#            --cap-add=SYS_ADMIN \
-#            --volume /lib/modules:/lib/modules:ro \
-#           --publish 8332:8332 \
-#           --publish 18332:18332 \
-#           --publish 18444:18444 \
-#           -it bsv_dev_ubuntu:20.04 /bin/bash
