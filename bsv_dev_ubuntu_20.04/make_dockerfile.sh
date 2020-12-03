45497304afd3afd15f9a3d7ea5a68ccca9c72434
Chris Gibson <chris@gibsonic.com>
2020-12-03 15:13:31 +0000

    fixed gdb

--- bsv_dev_ubuntu_20.04/make_dockerfile.sh
+++ bsv_dev_ubuntu_20.04/make_dockerfile.sh
@@ -3,14 +3,15 @@
 cat ../common/ubuntu_20.04 \
     ../common/tools \
     ../common/build_tools \
-    ../common/clang \
     ../common/misc \
     ../common/config \
+    ../common/bcc \
     ../common/bpftrace \
     ../common/perf \
+    ../common/gdb \
     dockerfile_ip \
     > dockerfile
 
 
-#    ../common/bcc \
+
 
