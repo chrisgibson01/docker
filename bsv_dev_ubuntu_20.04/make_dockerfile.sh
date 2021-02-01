d43d959cd65d3d36ebf20f9363e77d8a0ec0343d
Chris Gibson <chris@gibsonic.com>
2021-02-01 18:35:52 +0000

    config refactoring

--- bsv_dev_ubuntu_20.04/make_dockerfile.sh
+++ bsv_dev_ubuntu_20.04/make_dockerfile.sh
@@ -4,10 +4,10 @@ cat ../common/ubuntu_20.04 \
     ../common/tools \
     ../common/build_tools \
     ../common/misc \
-    ../common/config \
     ../common/perf \
     ../common/gdb \
     ../common/net_tools \
+    ../common/config \
     dockerfile_ip \
     > dockerfile
 
