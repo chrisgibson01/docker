799f3d8d9f6f1493c0367ab84455dfb212832303
Chris Gibson <chris@gibsonic.com>
2021-02-01 18:20:15 +0000

    bcc and bpftrace removed

--- bsv_dev_ubuntu_20.04/make_dockerfile.sh
+++ bsv_dev_ubuntu_20.04/make_dockerfile.sh
@@ -5,8 +5,6 @@ cat ../common/ubuntu_20.04 \
     ../common/build_tools \
     ../common/misc \
     ../common/config \
-    ../common/bcc \
-    ../common/bpftrace \
     ../common/perf \
     ../common/gdb \
     ../common/net_tools \
