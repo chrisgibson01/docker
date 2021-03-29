8e3cd9c091a0d6fdc2f4c5b286befe6c7d933967
Chris Gibson <chris@gibsonic.com>
2021-03-29 17:02:47 +0100

    pyflakes added

--- bsv_dev_ubuntu_20.04/make_dockerfile.sh
+++ bsv_dev_ubuntu_20.04/make_dockerfile.sh
@@ -8,6 +8,8 @@ cat ../common/ubuntu_20.04 \
     ../common/gdb \
     ../common/net_tools \
     ../common/clang \
+    ../common/vim \
+    ../common/python3 \
     ../common/config \
     dockerfile_ip \
     > dockerfile
